c2e4633df5780340003b157e637232e22b0af239
Chase Pierce <syntaqx@gmail.com>
2024-04-02 14:15:31 -0600

    testing customhex and nonhex

---
+++ pkg/decryptor/customhex_test.go
@@ -0,0 +1,47 @@
+package decryptor
+
+import (
+	"testing"
+
+	"github.com/syntaqx/ciphersprint/pkg/challenge"
+)
+
+func TestCustomHexDecryptor_Decrypt(t *testing.T) {
+	tests := []struct {
+		name                  string
+		encryptionMethod      string
+		encryptedPath         string
+		expectedDecryptedPath string
+		expectedError         error
+	}{
+		{
+			name:                  "Test 1",
+			encryptionMethod:      "encoded it with custom hex character set 157a9f204d3b86ec",
+			encryptedPath:         "task_7bbf368d45a6b1315474ce200008018e",
+			expectedDecryptedPath: "task_2bb5adc9813db0a01828fe67777c70ce",
+			expectedError:         nil,
+		},
+		// Add more test cases as needed
+	}
+
+	decryptor := CustomHexDecryptor{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			challengeResponse := challenge.ChallengeResponse{
+				EncryptionMethod: tt.encryptionMethod,
+				EncryptedPath:    tt.encryptedPath,
+			}
+
+			decryptedPath, err := decryptor.Decrypt(challengeResponse)
+
+			if err != tt.expectedError {
+				t.Errorf("Expected error: %v, got: %v", tt.expectedError, err)
+			}
+
+			if decryptedPath != tt.expectedDecryptedPath {
+				t.Errorf("Expected decrypted path: %s, got: %s", tt.expectedDecryptedPath, decryptedPath)
+			}
+		})
+	}
+}
---
+++ pkg/decryptor/nonhexfilter_test.go
@@ -0,0 +1,47 @@
+package decryptor
+
+import (
+	"testing"
+
+	"github.com/syntaqx/ciphersprint/pkg/challenge"
+)
+
+func TestNonHexFilterDecryptor_Decrypt(t *testing.T) {
+	decryptor := NonHexFilterDecryptor{}
+
+	tests := []struct {
+		name                  string
+		encryptionMethod      string
+		encryptedPath         string
+		expectedDecryptedPath string
+		expectedError         error
+	}{
+		{
+			name:                  "Test 1",
+			encryptionMethod:      "inserted some non-hex characters",
+			encryptedPath:         "task_i7aaa3d2397le03446481773d24e7abgkh06je",
+			expectedDecryptedPath: "task_7aaa3d2397e03446481773d24e7ab06e",
+			expectedError:         nil,
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			challengeResponse := challenge.ChallengeResponse{
+				EncryptionMethod: tt.encryptionMethod,
+				EncryptedPath:    tt.encryptedPath,
+			}
+
+			decryptedPath, err := decryptor.Decrypt(challengeResponse)
+
+			if err != tt.expectedError {
+				t.Errorf("Expected error: %v, got: %v", tt.expectedError, err)
+			}
+
+			if decryptedPath != tt.expectedDecryptedPath {
+				t.Errorf("Expected decrypted path: %s, got: %s", tt.expectedDecryptedPath, decryptedPath)
+			}
+		})
+	}
+}
