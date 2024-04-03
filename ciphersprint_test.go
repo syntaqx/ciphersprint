23bbdad6c42d396162ef22040bd72880f6c5afe7
Chase Pierce <syntaqx@gmail.com>
2024-04-03 11:43:29 -0600

    test error cases

--- ciphersprint_test.go
+++ ciphersprint_test.go
@@ -46,3 +46,31 @@ func TestClient_GetChallenge(t *testing.T) {
 		t.Errorf("Expected encrypted path: %s, got: %s", expectedChallengeResponse.EncryptedPath, challengeResponse.EncryptedPath)
 	}
 }
+
+func TestCliest_GetChallengeInvalidUrl(t *testing.T) {
+	client := NewClient(nil)
+	_, err := client.GetChallenge("!@#$%^&*()")
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestClient_GetChallengeInvalidResponse(t *testing.T) {
+	// Create a test server
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	}))
+	defer ts.Close()
+
+	client := NewClient(ts.Client())
+
+	// Set the baseURL to the client to the test server URL
+	baseURL, _ := url.Parse(ts.URL)
+	client.BaseURL = baseURL
+
+	// Call the GetChallenge method
+	_, err := client.GetChallenge("/challenge")
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
