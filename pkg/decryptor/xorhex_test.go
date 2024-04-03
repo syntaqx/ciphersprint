5df011b4c701637d31f4e7f6c809d79c36e55d51
Chase Pierce <syntaqx@gmail.com>
2024-04-03 11:02:05 -0600

    Update xorhex_test.go

--- pkg/decryptor/xorhex_test.go
+++ pkg/decryptor/xorhex_test.go
@@ -33,6 +33,10 @@ func TestXorHexDecryptor_Decrypt(t *testing.T) {
 				EncryptedPath:    tt.encryptedPath,
 			}
 
+      if !decryptor.Match(tt.challengeResponse) {
+				t.Errorf("Expected match to return true")
+			}
+      
 			decryptedPath, err := decryptor.Decrypt(challengeResponse)
 
 			if err != tt.expectedError {
